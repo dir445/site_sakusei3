316b4543a33b6dc51fd91c6ac3f93201c93f78c4
dir445 <ysnishino000@gmail.com>
2020-10-19 15:48:29 +0900

    10/19 検索フォーム文字数制限

--- js/videogallery.js
+++ js/videogallery.js
@@ -1,35 +1,46 @@
 console.log(APIKEY);
 
-const maxQueryLength = 100;
+const maxQueryLength = 1000;
 
-$('#query').on('keyup keydown keypress change',function() {
-    console.log($(this).val().length);
-});
+$(function() {
+    showQueryLength(0);
 
-$('#search > input[type="submit"]').on('click', function(e) {
-    console.log("a");
-    e.preventDefault();
-
-    let url = 'https://www.googleapis.com/youtube/v3/search?';    
-    url += 'type=video';
-    url += '&part=snippet';
-    url += '&q=' + $('#query').val();
-    url += '&videoEmbeddable=true';
-    url += '&videoSyndicated=true';
-    url += '&maxResults=6';
-    url += '&key=' + APIKEY;
-    
-    console.log(url);
-
-    $.ajax({
-        url: url,
-        dataType: 'jsonp'
-    }).done(showVideos)
-    .fail((data)=> {
-        showMessage('通信に失敗しました');
+    $('#query').on('keyup keydown keypress change',function() {
+        const length = $(this).val().length;
+        if(length > maxQueryLength) {
+            $(this).val($(this).val().slice(0,maxQueryLength));
+            showQueryLength(maxQueryLength);
+            return;
+        }
+        showQueryLength(length);
     });    
-    return false;
+    
+    $('#search > input[type="submit"]').on('click', function(e) {
+        e.preventDefault();
+    
+        let url = 'https://www.googleapis.com/youtube/v3/search?';    
+        url += 'type=video';
+        url += '&part=snippet';
+        url += '&q=' + $('#query').val();
+        url += '&videoEmbeddable=true';
+        url += '&videoSyndicated=true';
+        url += '&maxResults=6';
+        url += '&key=' + APIKEY;
+    
+        $.ajax({
+            url: url,
+            dataType: 'jsonp'
+        }).done(showVideos)
+        .fail((data)=> {
+            showMessage('通信に失敗しました');
+        });    
+        return false;
+    });    
 });
+
+function showQueryLength(length) {
+    $('#queryLength').text(length + '/' + maxQueryLength);
+}
 
 function showVideos(data){
     if(!data.items){
